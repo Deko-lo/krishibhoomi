5185e62b7d98fd892732a18fabde2365326d312a
Pranshul Jain <pranshuljain103@gmail.com>
2026-09-07 23:55:02 +0530

    Prepare app for deployment

--- backend/server.js
+++ backend/server.js
@@ -223,8 +223,8 @@ app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html");
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
     console.log(`KrishiBhoomi Backend running on http://localhost:${PORT}`);
 });
